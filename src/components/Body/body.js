c836e8884550c38be8b2277da19cfd45f63ff321
unknown <akshaymatrecode@gmail.com>
2023-05-17 23:17:12 +0530

    Chips for multiple data in the expreience section

--- src/components/Body/body.js
+++ src/components/Body/body.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Body.module.css"
 import {Download} from "react-feather"
 import Editor from '../Editor/Editor'
 const Body = () => {
     const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936" ]
+
+    // Object for form sections
     const sections = {
         basicInfo : "Basic Info",
         workExp : "Work Experience",
@@ -13,6 +15,50 @@ const Body = () => {
         summary: "Summary",
         other : "Other",
     }
+
+    // Takes form data and store all information in the state and retrieve and update the state according to the data
+    const [cvInformation, setCvInformation] = useState({
+        [sections.basicInfo]:{
+            id:sections.basicInfo,
+            title:sections.basicInfo,
+            detail:{}
+        },
+
+        [sections.workExp]:{
+            id:sections.workExp,
+            title:sections.workExp,
+            details:[{Exp:"Hello"}]
+        },
+
+        [sections.projects]:{
+            id:sections.projects,
+            title:sections.projects,
+            details:[]
+        },
+        [sections.education]:{
+            id:sections.education,
+            title:sections.education,
+            details:[]
+        },
+
+        [sections.achievements]:{
+            id:sections.achievements,
+            title:sections.achievements,
+            points:[]
+        },
+        [sections.summary]:{
+            id:sections.summary,
+            title:sections.summary,
+            details:""
+        },
+
+        [sections.other]:{
+            id:sections.other,
+            title:sections.other,
+            details:""
+        },
+
+    })
   return (
     <div className={styles.container}>
         <div className={styles.heading}>
@@ -29,7 +75,7 @@ const Body = () => {
             <button className={styles.downloadButton}>Download <Download/></button>
         </div>
         <div className={styles.main}>
-            <Editor sections={sections}/>
+            <Editor sections={sections} information = {cvInformation}/>
         </div>
        
 
